404f478000393cc9fb4af2a06f99173d1ca43ce2
Max Cherry <mxmCherry@gmail.com>
2015-04-30 20:06:59 +0300

    Site-specific types and constants

--- 2.3/rtb/rtb.go
+++ 2.3/rtb/rtb.go
@@ -7,10 +7,10 @@ package rtb
 // Gender, where “M” = male, “F” = female, “O” = known to be
 //   other (i.e., omitted is unknown).
 const (
-	UserGenderMale    string = "M"
-	UserGenderFemale  string = "F"
-	UserGenderOther   string = "O"
-	UserGenderUnknown string = ""
+	UserGenderMale    string = "M" // “M” = male
+	UserGenderFemale  string = "F" // “F” = female
+	UserGenderOther   string = "O" // “O” = known to be other
+	UserGenderUnknown string = ""  // omitted is unknown
 )
 
 
@@ -23,8 +23,455 @@ const (
 )
 
 
+// Mobile-optimized signal, where 0 = no, 1 = yes.
+const (
+	SiteMobileNo  uint8 = 0 // 0 = no
+	SiteMobileYes uint8 = 1 // 1 = yes
+)
+
+
+// Indicates if the site has a privacy policy, where 0 = no, 1 = yes.
+const (
+	SitePrivacyPolicyNo  uint8 = 0 // 0 = no
+	SitePrivacyPolicyYes uint8 = 1 // 1 = yes
+)
+
+
+// 0 = not live, 1 = content is live (e.g., stream, live blog).
+const (
+	ContentLiveStreamNo  uint8 = 0 // 0 = not live
+	ContentLiveStreamYes uint8 = 1 // 1 = content is live (e.g., stream, live blog)
+)
+
+
+// 0 = indirect, 1 = direct.
+const (
+	ContentSourceRelationshipIndirect uint8 = 0 // 0 = indirect
+	ContentSourceRelationshipDirect   uint8 = 1 // 1 = direct
+)
+
+// Indicator of whether or not the content is embeddable (e.g.,
+// an embeddable video player), where 0 = no, 1 = yes.
+const (
+	ContentEmbeddableNo  uint8 = 0 // 0 = no
+	ContentEmbeddableYes uint8 = 1 // 1 = yes
+)
+
+
 // Placeholder for exchange-specific extensions to OpenRTB.
 type Ext interface {}
+
+
+// 3.2.6 Object: Site
+//   This object should be included if the ad supported content is a website as opposed to a non-browser
+//   application. A bid request must not contain both a Site and an App object. At a minimum, it is useful
+//   to provide a site ID or page URL, but this is not strictly required.
+type Site struct {
+	
+	// Attribute:
+	//   id
+	// Type:
+	//   string; recommended
+	// Description:
+	//   Exchange-specific site ID.
+	ID string `json:"id"`
+	
+	// Attribute:
+	//   name
+	// Type:
+	//   string
+	// Description:
+	//   Site name (may be aliased at the publisher’s request).
+	Name string `json:"name"`
+	
+	// Attribute:
+	//   domain
+	// Type:
+	//   string
+	// Description:
+	//   Domain of the site (e.g., “mysite.foo.com”).
+	Domain string `json:"domain"`
+	
+	// Attribute:
+	//   cat
+	// Type:
+	//   string array
+	// Description:
+	//   Array of IAB content categories of the site. Refer to List 5.1.
+	Cat []string `json:"cat"`
+	
+	// Attribute:
+	//   sectioncat
+	// Type:
+	//   string array
+	// Description:
+	//   Array of IAB content categories that describe the current
+	//   section of the site. Refer to List 5.1.
+	SectionCat []string `json:"sectioncat"`
+	
+	// Attribute:
+	//   pagecat
+	// Type:
+	//   string array
+	// Description:
+	//   Array of IAB content categories that describe the current page
+	//   or view of the site. Refer to List 5.1.
+	PageCat []string `json:"pagecat"`
+	
+	// Attribute:
+	//   page
+	// Type:
+	//   string
+	// Description:
+	//   URL of the page where the impression will be shown.
+	Page string `json:"page"`
+	
+	// Attribute:
+	//   ref
+	// Type:
+	//   string
+	// Description:
+	//   Referrer URL that caused navigation to the current page.
+	Ref string `json:"ref"`
+	
+	// Attribute:
+	//   search
+	// Type:
+	//   string
+	// Description:
+	//   Search string that caused navigation to the current page.
+	Search string `json:"search"`
+	
+	// Attribute:
+	//   mobile
+	// Type:
+	//   integer
+	// Description:
+	//   Mobile-optimized signal, where 0 = no, 1 = yes.
+	Mobile uint8 `json:"mobile"`
+	
+	// Attribute:
+	//   privacypolicy
+	// Type:
+	//   integer
+	// Description:
+	//   Indicates if the site has a privacy policy, where 0 = no, 1 = yes.
+	PrivacyPolicy uint8 `json:"privacypolicy"`
+	
+	// Attribute:
+	//   publisher
+	// Type:
+	//   object
+	// Description:
+	//   Details about the Publisher (Section 3.2.8) of the site.
+	Publisher Publisher `json:"publisher"`
+	
+	// Attribute:
+	//   content
+	// Type:
+	//   object
+	// Description:
+	//   Details about the Content (Section 3.2.9) within the site.
+	Content Content `json:"content"`
+	
+	// Attribute:
+	//   keywords
+	// Type:
+	//   string
+	// Description:
+	//   Comma separated list of keywords about the site.
+	Keywords string `json:"keywords"`
+	
+	// Attribute:
+	//   ext
+	// Type:
+	//   object
+	// Description:
+	//   Placeholder for exchange-specific extensions to OpenRTB.
+	Ext Ext `json:"ext"`
+	
+}
+
+
+// 3.2.8 Object: Publisher
+//   This object describes the publisher of the media in which the ad will be displayed. The publisher is
+//   typically the seller in an OpenRTB transaction.
+type Publisher struct {
+	
+	// Attribute:
+	//   id
+	// Type:
+	//   string
+	// Description:
+	//   Exchange-specific publisher ID.
+	ID string `json:"id"`
+	
+	// Attribute:
+	//   name
+	// Type:
+	//   string
+	// Description:
+	//   Publisher name (may be aliased at the publisher’s request).
+	Name string `json:"name"`
+	
+	// Attribute:
+	//   cat
+	// Type:
+	//   string array
+	// Description:
+	//   Array of IAB content categories that describe the publisher.
+	//   Refer to List 5.1.
+	Cat []string `json:"cat"`
+	
+	// Attribute:
+	//   domain
+	// Type:
+	//   string
+	// Description:
+	//   Highest level domain of the publisher (e.g., “publisher.com”).
+	Domain string `json:"domain"`
+	
+	// Attribute:
+	//   ext
+	// Type:
+	//   object
+	// Description:
+	//   Placeholder for exchange-specific extensions to OpenRTB.
+	Ext Ext `json:"ext"`
+	
+}
+
+
+// 3.2.9 Object: Content
+//   This object describes the content in which the impression will appear, which may be syndicated or nonsyndicated
+//   content. This object may be useful when syndicated content contains impressions and does
+//   not necessarily match the publisher’s general content. The exchange might or might not have
+//   knowledge of the page where the content is running, as a result of the syndication method. For
+//   example might be a video impression embedded in an iframe on an unknown web property or device.
+type Content struct {
+	
+	// Attribute:
+	//   id
+	// Type:
+	//   string
+	// Description:
+	//   ID uniquely identifying the content.
+	ID string `json:"id"`
+	
+	// Attribute:
+	//   episode
+	// Type:
+	//   integer
+	// Description:
+	//   Episode number (typically applies to video content).
+	Episode uint64 `json:"episode"`
+	
+	// Attribute:
+	//   title
+	// Type:
+	//   string
+	// Description:
+	//   Content title.
+	//   Video Examples: “Search Committee” (television), “A New
+	//   Hope” (movie), or “Endgame” (made for web).
+	//   Non-Video Example: “Why an Antarctic Glacier Is Melting So
+	//   Quickly” (Time magazine article).
+	Title string `json:"title"`
+	
+	// Attribute:
+	//   series
+	// Type:
+	//   string
+	// Description:
+	//   Content series.
+	//   Video Examples: “The Office” (television), “Star Wars” (movie),
+	//   or “Arby ‘N’ The Chief” (made for web).
+	//   Non-Video Example: “Ecocentric” (Time Magazine blog).
+	Series string `json:"series"`
+	
+	// Attribute:
+	//   season
+	// Type:
+	//   string
+	// Description:
+	//   Content season; typically for video content (e.g., “Season 3”).
+	Season string `json:"season"`
+	
+	// Attribute:
+	//   producer
+	// Type:
+	//   object
+	// Description:
+	//   Details about the content Producer (Section 3.2.10).
+	Producer Producer `json:"producer"`
+	
+	// Attribute:
+	//   url
+	// Type:
+	//   string
+	// Description:
+	//   URL of the content, for buy-side contextualization or review.
+	URL string `json:"url"`
+	
+	// Attribute:
+	//   cat
+	// Type:
+	//   string array
+	// Description:
+	//   Array of IAB content categories that describe the content
+	//   producer. Refer to List 5.1.
+	Cat []string `json:"cat"`
+	
+	// Attribute:
+	//   videoquality
+	// Type:
+	//   integer
+	// Description:
+	//   Video quality per IAB’s classification. Refer to List 5.11.
+	VideoQuality uint64 `json:"videoquality"`
+	
+	// Attribute:
+	//   context
+	// Type:
+	//   integer
+	// Description:
+	//   Type of content (game, video, text, etc.). Refer to List 5.14.
+	Context uint64 `json:"context"`
+	
+	// Attribute:
+	//   contentrating
+	// Type:
+	//   string
+	// Description:
+	//   Content rating (e.g., MPAA).
+	ContentRating string `json:"contentrating"`
+	
+	// Attribute:
+	//   userrating
+	// Type:
+	//   string
+	// Description:
+	//   User rating of the content (e.g., number of stars, likes, etc.).
+	UserRating string `json:"userrating"`
+	
+	// Attribute:
+	//   qagmediarating
+	// Type:
+	//   integer
+	// Description:
+	//   Media rating per QAG guidelines. Refer to List 5.15.
+	QAGMediaRating uint64 `json:"qagmediarating"`
+	
+	// Attribute:
+	//   keywords
+	// Type:
+	//   string
+	// Description:
+	//   Comma separated list of keywords describing the content.
+	Keywords string `json:"keywords"`
+	
+	// Attribute:
+	//   livestream
+	// Type:
+	//   integer
+	// Description:
+	//   0 = not live, 1 = content is live (e.g., stream, live blog).
+	LiveStream uint8 `json:"livestream"`
+	
+	// Attribute:
+	//   sourcerelationship
+	// Type:
+	//   integer
+	// Description:
+	//   0 = indirect, 1 = direct.
+	SourceRelationship uint8 `json:"sourcerelationship"`
+	
+	// Attribute:
+	//   len
+	// Type:
+	//   integer
+	// Description:
+	//   Length of content in seconds; appropriate for video or audio.
+	Len uint64 `json:"len"`
+	
+	// Attribute:
+	//   language
+	// Type:
+	//   string
+	// Description:
+	//   Content language using ISO-639-1-alpha-2.
+	Language string `json:"language"`
+	
+	// Attribute:
+	//   embeddable
+	// Type:
+	//   integer
+	// Description:
+	//   Indicator of whether or not the content is embeddable (e.g.,
+	//   an embeddable video player), where 0 = no, 1 = yes.
+	Embeddable uint8 `json:"embeddable"`
+	
+	// Attribute:
+	//   ext
+	// Type:
+	//   object
+	// Description:
+	//   Placeholder for exchange-specific extensions to OpenRTB.
+	Ext Ext `json:"ext"`
+	
+}
+
+
+// 3.2.10 Object: Producer
+//   This object defines the producer of the content in which the ad will be shown. This is particularly useful
+//   when the content is syndicated and may be distributed through different publishers and thus when the
+//   producer and publisher are not necessarily the same entity.
+type Producer struct {
+	
+	// Attribute:
+	//   id
+	// Type:
+	//   string
+	// Description:
+	//   Content producer or originator ID. Useful if content is
+	//   syndicated and may be posted on a site using embed tags.
+	ID string `json:"id"`
+	
+	// Attribute:
+	//   name
+	// Type:
+	//   string
+	// Description:
+	//   Content producer or originator name (e.g., “Warner Bros”).
+	Name string `json:"name"`
+	
+	// Attribute:
+	//   cat
+	// Type:
+	//   string array
+	// Description:
+	//   Array of IAB content categories that describe the content
+	//   producer. Refer to List 5.1.
+	Cat []string `json:"cat"`
+	
+	// Attribute:
+	//   domain
+	// Type:
+	//   string
+	// Description:
+	//   Highest level domain of the content producer (e.g.,
+	//   “producer.com”).
+	Domain string `json:"domain"`
+	
+	// Attribute:
+	//   ext
+	// Type:
+	//   object
+	// Description:
+	//   Placeholder for exchange-specific extensions to OpenRTB.
+	Ext Ext `json:"ext"`
+	
+}
 
 
 // 3.2.12 Object: Geo
