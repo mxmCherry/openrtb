20dc311b235f2b01dedd1be313591f58dc0c308a
Max Cherry <mxmCherry@gmail.com>
2017-10-19 21:21:04 +0300

    Added Ptr{Int8,Uint64} helpers

---
+++ ptr.go
@@ -0,0 +1,7 @@
+package openrtb
+
+// PtrInt8 returns pointer to passed number argument.
+func PtrInt8(n int8) *int8 { return &n }
+
+// PtrUint64 returns pointer to passed number argument.
+func PtrUint64(n uint64) *uint64 { return &n }
