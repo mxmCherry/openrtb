8408fda57a92017cd1ee0eb57160f63382843e4a
Max Cherry <mxmCherry@gmail.com>
2017-11-13 20:29:54 +0200

    Test OCD cosmetics

--- openrtb_test.go
+++ openrtb_test.go
@@ -6,7 +6,7 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/mxmCherry/openrtb"
+	. "github.com/mxmCherry/openrtb"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
@@ -37,42 +37,42 @@ var _ = DescribeTable(
 	Entry(
 		"Bid Request - Simple Banner",
 		"bid-request-simple-banner.json",
-		new(openrtb.BidRequest)),
+		new(BidRequest)),
 	Entry(
 		"Bid Request - Expandable Creative",
 		"bid-request-expandable-creative.json",
-		new(openrtb.BidRequest)),
+		new(BidRequest)),
 	Entry(
 		"Bid Request - Mobile",
 		"bid-request-mobile.json",
-		new(openrtb.BidRequest)),
+		new(BidRequest)),
 	Entry(
 		"Bid Request - Video",
 		"bid-request-video.json",
-		new(openrtb.BidRequest)),
+		new(BidRequest)),
 	Entry(
 		"Bid Request - PMP with Direct Deal",
 		"bid-request-pmp-with-direct-deal.json",
-		new(openrtb.BidRequest)),
+		new(BidRequest)),
 	Entry(
 		"Bid Request - Native Ad",
 		"bid-request-native-ad.json",
-		new(openrtb.BidRequest)),
+		new(BidRequest)),
 
 	Entry(
 		"Bid Response - Ad Served on Win Notice",
 		"bid-response-ad-served-on-win-notice.json",
-		new(openrtb.BidResponse)),
+		new(BidResponse)),
 	Entry(
 		"Bid Response - VAST XML Document Returned Inline",
 		"bid-response-vast-xml-document-returned-inline.json",
-		new(openrtb.BidResponse)),
+		new(BidResponse)),
 	Entry(
 		"Bid Response - Direct Deal Ad Served on Win Notice",
 		"bid-response-direct-deal-ad-served-on-win-notice.json",
-		new(openrtb.BidResponse)),
+		new(BidResponse)),
 	Entry(
 		"Bid Response - Native Markup Returned Inline",
 		"bid-response-native-markup-returned-inline.json",
-		new(openrtb.BidResponse)),
+		new(BidResponse)),
 )
