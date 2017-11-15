3b32f823ae1a6a802ba3aeaca48c51f2676c21a3
Max Cherry <mxmCherry@gmail.com>
2017-11-15 21:10:15 +0200

    native/request: ContextSubType comment cosmetics + Marketplace is an AppStore really

--- native/request/context_sub_type.go
+++ native/request/context_sub_type.go
@@ -12,14 +12,14 @@ const (
 	ContextSubTypeGeneral       ContextSubType = 10 // General or mixed content.
 	ContextSubTypeArticle       ContextSubType = 11 // Primarily article content (which of course could include images, etc as part of the article)
 	ContextSubTypeVideo         ContextSubType = 12 // Primarily video content
-	ContextSubTypeAudio         ContextSubType = 13 //  Primarily audio content
+	ContextSubTypeAudio         ContextSubType = 13 // Primarily audio content
 	ContextSubTypeImage         ContextSubType = 14 // Primarily image content
 	ContextSubTypeUserGenerated ContextSubType = 15 // User-generated content - forums, comments, etc
 	ContextSubTypeSocial        ContextSubType = 20 // General social content such as a general social network
 	ContextSubTypeEmail         ContextSubType = 21 // Primarily email content
 	ContextSubTypeChat          ContextSubType = 22 // Primarily chat/IM content
 	ContextSubTypeSelling       ContextSubType = 30 // Content focused on selling products, whether digital or physical
-	ContextSubTypeMarketplace   ContextSubType = 31 // Application store/marketplace
+	ContextSubTypeAppStore      ContextSubType = 31 // Application store/marketplace
 	ContextSubTypeProductReview ContextSubType = 32 // Product reviews site primarily (which may sell product secondarily)
 
 	// 500+ To be defined by the exchange
