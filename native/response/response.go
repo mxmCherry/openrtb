2ac487edb15f7a79aaaae7a740313d95bd4b0c70
Max Cherry <mxmCherry@gmail.com>
2018-02-25 18:00:52 +0200

    Native 1.2: 5.1 Native Markup Response Object reviewed

--- native/response/response.go
+++ native/response/response.go
@@ -11,11 +11,11 @@ type Response struct {
 	// Field:
 	//   ver
 	// Scope:
-	//   optional
+	//   recommended
 	// Type:
 	//   string
 	// Default:
-	//   1.1
+	//   1.2
 	// Description:
 	//   Version of the Native Markup version in use.
 	Ver string `json:"ver,omitempty"`
@@ -23,12 +23,38 @@ type Response struct {
 	// Field:
 	//   assets
 	// Scope:
-	//   required
+	//   recommended
 	// Type:
 	//   object array
 	// Description:
 	//   List of native ad’s assets.
-	Assets []Asset `json:"assets"`
+	//   Required if no assetsurl.
+	//   Recommended as fallback even if assetsurl is provided.
+	Assets []Asset `json:"assets,omitempty"`
+
+	// Field:
+	//   assetsurl
+	// Scope:
+	//   optional
+	// Type:
+	//   string
+	// Description:
+	//   URL of an alternate source for the assets object.
+	//   The expected response is a JSON object mirroring the assets object in the bid response, subject to certain requirements as specified in the individual objects.
+	//   Where present, overrides the asset object in the response.
+	AssetsURL string `json:"assetsurl,omitempty"`
+
+	// Field:
+	//   dcourl
+	// Scope:
+	//   optional
+	// Type:
+	//   string
+	// Description:
+	//   URL where a dynamic creative specification may be found for populating this ad, per the Dynamic Content Ads Specification.
+	//   Note this is a beta option as the interpretation of the Dynamic Content Ads Specification and how to assign those elements into a native ad is outside the scope of this spec and must be agreed offline between the parties or as may be specified in a future revision of the Dynamic Content Ads spec.
+	//   Where present, overrides the asset object in the response.
+	DCOURL string `json:"dcourl,omitempty"`
 
 	// Field:
 	//   link
@@ -37,7 +63,8 @@ type Response struct {
 	// Type:
 	//   object
 	// Description:
-	//   Destination Link. This is default link object for the ad.
+	//   Destination Link.
+	//   This is default link object for the ad.
 	//   Individual assets can also have a link object which applies if the asset is activated(clicked).
 	//   If the asset doesn’t have a link object, the parent link object applies.
 	Link Link `json:"link"`
@@ -49,8 +76,8 @@ type Response struct {
 	// Type:
 	//   string array
 	// Description:
-	//   Array of impression tracking URLs, expected to return a 1x1 image or 204 response - typically
-	//   only passed when using 3rd party trackers.
+	//   Array of impression tracking URLs, expected to return a 1x1 image or 204 response - typically only passed when using 3rd party trackers.
+	//   To be deprecated - replaced with eventtrackers.
 	ImpTrackers []string `json:"imptrackers,omitempty"`
 
 	// Field:
@@ -60,9 +87,32 @@ type Response struct {
 	// Type:
 	//   string
 	// Description:
-	//   Optional JavaScript impression tracker. This is a valid HTML, Javascript is already wrapped in <script> tags.
+	//   Optional JavaScript impression tracker.
+	//   This is a valid HTML, Javascript is already wrapped in <script> tags.
 	//   It should be executed at impression time where it can be supported.
+	//   To be deprecated - replaced with eventtrackers.
 	JSTracker string `json:"jstracker,omitempty"`
+
+	// Field:
+	//   eventtrackers
+	// Scope:
+	//   optional
+	// Type:
+	//   Array of objects
+	// Description:
+	//   Array of tracking objects to run with the ad, in response to the declared supported methods in the request.
+	//   Replaces imptrackers and jstracker, to be deprecated.
+	EventTrackers []interface{} `json:"eventtrackers,omitempty"` // TODO: use Tracker type
+
+	// Field:
+	//   privacy
+	// Scope:
+	//   optional
+	// Type:
+	//   string
+	// Description:
+	//   If support was indicated in the request, URL of a page informing the user about the buyer’s targeting activity.
+	Privacy string `json:"privacy,omitempty"`
 
 	// Field:
 	//   ext
