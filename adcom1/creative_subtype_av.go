826b155bb225310124fee562b8d4117355064514
Max Cherry <mxmCherry@gmail.com>
2018-12-30 12:58:07 +0200

    adcom1: use zeros in proto version constants
    Seems a bit more natural + it already distinguishes between stuff like 4.0 and 4.1

--- adcom1/creative_subtype_av.go
+++ adcom1/creative_subtype_av.go
@@ -5,16 +5,16 @@ type CreativeSubtypeAV int8 // TODO: rename to smth like AudioVideoCreativeSubty
 
 // Subtypes of audio and video ad creatives.
 const (
-	CreativeVAST1         CreativeSubtypeAV = 1  // VAST 1.0
-	CreativeVAST2         CreativeSubtypeAV = 2  // VAST 2.0
-	CreativeVAST3         CreativeSubtypeAV = 3  // VAST 3.0
-	CreativeVAST1Wrapper  CreativeSubtypeAV = 4  // VAST 1.0 Wrapper
-	CreativeVAST2Wrapper  CreativeSubtypeAV = 5  // VAST 2.0 Wrapper
-	CreativeVAST3Wrapper  CreativeSubtypeAV = 6  // VAST 3.0 Wrapper
-	CreativeVAST4         CreativeSubtypeAV = 7  // VAST 4.0
-	CreativeVAST4Wrapper  CreativeSubtypeAV = 8  // VAST 4.0 Wrapper
-	CreativeDAAST1        CreativeSubtypeAV = 9  // DAAST 1.0
-	CreativeDAAST1Wrapper CreativeSubtypeAV = 10 // DAAST 1.0 Wrapper
-	CreativeVAST41        CreativeSubtypeAV = 11 // VAST 4.1
-	CreativeVAST41Wrapper CreativeSubtypeAV = 12 // VAST 4.1 Wrapper
+	CreativeVAST10         CreativeSubtypeAV = 1  // VAST 1.0
+	CreativeVAST20         CreativeSubtypeAV = 2  // VAST 2.0
+	CreativeVAST30         CreativeSubtypeAV = 3  // VAST 3.0
+	CreativeVAST10Wrapper  CreativeSubtypeAV = 4  // VAST 1.0 Wrapper
+	CreativeVAST20Wrapper  CreativeSubtypeAV = 5  // VAST 2.0 Wrapper
+	CreativeVAST30Wrapper  CreativeSubtypeAV = 6  // VAST 3.0 Wrapper
+	CreativeVAST40         CreativeSubtypeAV = 7  // VAST 4.0
+	CreativeVAST40Wrapper  CreativeSubtypeAV = 8  // VAST 4.0 Wrapper
+	CreativeDAAST10        CreativeSubtypeAV = 9  // DAAST 1.0
+	CreativeDAAST10Wrapper CreativeSubtypeAV = 10 // DAAST 1.0 Wrapper
+	CreativeVAST41         CreativeSubtypeAV = 11 // VAST 4.1
+	CreativeVAST41Wrapper  CreativeSubtypeAV = 12 // VAST 4.1 Wrapper
 )
