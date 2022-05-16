1d20ff2e69071c11d6dc1343070c4e1811bbc6db
mxmCherry <mxmCherry@users.noreply.github.com>
2022-05-16 20:54:05 +0300

    adcom1: change const prefix ATypeXXX -> AgentTypeXXX

--- adcom1/agent_type.go
+++ adcom1/agent_type.go
@@ -7,7 +7,7 @@ type AgentType int64
 //
 // Values of 500+ hold vendor-specific codes.
 const (
-	ATypeWeb    AgentType = 1 // An ID which is tied to a specific web browser or device (cookie-based, probabilistic, or other).
-	ATypeApp    AgentType = 2 // In-app impressions, which will typically contain a type of device ID (or rather, the privacy-compliant versions of device IDs).
-	ATypePerson AgentType = 3 // A person-based ID, i.e., that is the same across devices.
+	AgentTypeWeb    AgentType = 1 // An ID which is tied to a specific web browser or device (cookie-based, probabilistic, or other).
+	AgentTypeApp    AgentType = 2 // In-app impressions, which will typically contain a type of device ID (or rather, the privacy-compliant versions of device IDs).
+	AgentTypePerson AgentType = 3 // A person-based ID, i.e., that is the same across devices.
 )
