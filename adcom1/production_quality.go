185f7cb397059fdec4be058462fb8f5e3aa610bb
mxmCherry <mxmCherry@gmail.com>
2022-05-19 15:54:31 +0300

    adcom1: prod quality - add Unknown (0) - defined by recent AdCOM

--- adcom1/production_quality.go
+++ adcom1/production_quality.go
@@ -6,6 +6,7 @@ type ProductionQuality int8
 // Options for content quality.
 // These values are defined by the IAB; refer to www.iab.com/wp-content/uploads/2015/03/long-form-video-final.pdf for more information.
 const (
+	ProductionUnknown      ProductionQuality = 0 // Unknown
 	ProductionProfessional ProductionQuality = 1 // Professionally Produced
 	ProductionProsumer     ProductionQuality = 2 // Prosumer
 	ProductionUser         ProductionQuality = 3 // User Generated (UGC)
