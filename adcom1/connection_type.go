29db7b87851dde1cef2e233e555d1c49135f946f
Scott Kay <noreply@syntaxnode.com>
2022-06-21 16:27:45 -0400

    openrtb2 connectiontype should still be a pointer

--- adcom1/connection_type.go
+++ adcom1/connection_type.go
@@ -5,6 +5,7 @@ type ConnectionType int8
 
 // Options for the type of device connectivity.
 const (
+	ConnectionUnknown  ConnectionType = 0 // 0 Unknown
 	ConnectionEthernet ConnectionType = 1 // 1	Ethernet; Wired Connection
 	ConnectionWIFI     ConnectionType = 2 // 2	WIFI
 	ConnectionCellular ConnectionType = 3 // 3	Cellular Network - Unknown Generation
@@ -13,3 +14,16 @@ const (
 	Connection4G       ConnectionType = 6 // 6	Cellular Network - 4G
 	Connection5G       ConnectionType = 7 // 7	Cellular Network - 5G
 )
+
+// Ptr returns pointer to own value.
+func (c ConnectionType) Ptr() *ConnectionType {
+	return &c
+}
+
+// Val safely dereferences pointer, returning default value (ConnectionUnknown) for nil.
+func (c *ConnectionType) Val() ConnectionType {
+	if c == nil {
+		return ConnectionUnknown
+	}
+	return *c
+}
